a4665204272021bcaef312f1e622d37e4c74ab96
Hongbin <honcao@microsoft.com>
2018-04-23 17:20:43 -0700

    changes

--- main.go
+++ main.go
@@ -24,22 +24,22 @@ func main() {
 	os.Setenv("APIVERSION_ARM_STORAGE", "2016-01-01")
 	os.Setenv("APIVERSION_STORAGE", "2016-05-31")
 	resourceManagerEndpoint := os.Getenv("RESOURCEMANAGERENDPOINT") // "https://management.local.azurestack.external"
-	fmt.Println("abc")
-	fmt.Println(os.Getenv("SUBSCRIPTIONID"))
-	subscriptionID := os.Getenv("SUBSCRIPTIONID")                       //"110054c2-21bc-442c-b214-c31c2578a371"
-	serviceManagementEndpoint := os.Getenv("SERVICEMANAGEMENTENDPOINT") //"https://management.azurestackci11.onmicrosoft.com/8d887891-6596-46c4-bdb6-1fbde1edbc7e"
-	tenantID := os.Getenv("TENANTID")                                   //"d9b782d5-d098-4374-8f2c-3907cc34611c"
-	activeDirectoryEndpoint := os.Getenv("ACTIVEDIRECTORYENDPOINT")     //"https://login.windows.net/"
-	aADClientID := os.Getenv("AADCLIENTID")                             //"a7a77abf-ad26-4bb8-9abd-329d03d14804"
-	aADClientSecret := os.Getenv("AADCLIENTSECRET")                     //"7iiAzT+66U3zazrlnZjNwAVqf7tFscThEVOx1TrGunc="
+	fmt.Printf("abc")
+	fmt.Printf(os.Getenv("SUBSCRIPTIONID"))
+	subscriptionID := "2b0feee4-6113-4b72-a101-a05237d923e9"                                                              //os.Getenv("SUBSCRIPTIONID")                       //"110054c2-21bc-442c-b214-c31c2578a371"
+	serviceManagementEndpoint := "https://management.azurestackci06.onmicrosoft.com/53f9b9db-9b09-47e7-9932-7740fbea635a" //os.Getenv("SERVICEMANAGEMENTENDPOINT") //"https://management.azurestackci11.onmicrosoft.com/8d887891-6596-46c4-bdb6-1fbde1edbc7e"
+	tenantID := "5308332c-26e2-4fdb-9beb-e883a706bc08"                                                                    // os.Getenv("TENANTID")                                                                                     //"d9b782d5-d098-4374-8f2c-3907cc34611c"
+	activeDirectoryEndpoint := "https://login.windows.net/"                                                               // os.Getenv("ACTIVEDIRECTORYENDPOINT")                                                       //"https://login.windows.net/"
+	aADClientID := "e0d778c3-6db3-4c15-947e-9a66cb001b59"                                                                 //os.Getenv("AADCLIENTID")                                                                               //"a7a77abf-ad26-4bb8-9abd-329d03d14804"
+	aADClientSecret := "O28MDuzu8+J21PTPooGqIeEfIY+PlnWw4Lr4XwJQPS8="                                                     //os.Getenv("AADCLIENTSECRET")                                                                       //"7iiAzT+66U3zazrlnZjNwAVqf7tFscThEVOx1TrGunc="
 	servicePrincipalToken, _ := GetServicePrincipalToken(activeDirectoryEndpoint, tenantID, serviceManagementEndpoint, aADClientID, aADClientSecret)
 
 	storageAccountClient := storage.NewAccountsClientWithBaseURI(resourceManagerEndpoint, subscriptionID)
 	storageAccountClient.Authorizer = autorest.NewBearerAuthorizer(servicePrincipalToken)
 	configureUserAgent(&storageAccountClient.Client)
 
-	SAName := os.Getenv("SANAME")               //"pvc2068417454001"
-	resourceGroup := os.Getenv("RESOURCEGROUP") //"kl214"
+	SAName := "storagetest"        //os.Getenv("SANAME")               //"pvc2068417454001"
+	resourceGroup := "storagetest" // os.Getenv("RESOURCEGROUP") //"kl214"
 	listKeysResult, err := storageAccountClient.ListKeys(resourceGroup, SAName)
 
 	if err != nil {
